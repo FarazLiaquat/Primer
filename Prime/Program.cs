ff5998a7e989c723768a8728563287b93ae3aab6
Faraz Liaquat <farazlikesfire@gmail.com>
2021-09-10 16:12:03 -0400

    Primer

---
+++ Prime/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z;
+            for (int x = 1; x <= 1000; x++)
+            {
+                z = 0;
+                for (int y = 2; y <= x/2; y++)
+                { 
+                    if (x % y == 0)
+                    {
+                        z++;
+                         
+                        break;
+                    }
+                }
+
+                if (z == 0 && x != 1)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
+
